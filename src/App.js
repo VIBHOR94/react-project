df3d6fd5731a7d32d73f0c18c621c29e53a870b4
VIBHOR94 <vibhorgupta2008@aol.in>
2018-08-18 02:39:04 +0530

    Adding method reference as property to another component

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
         <p> This is really working.</p>
         <button onClick={this.switchNameHandler}>Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler}>My Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
