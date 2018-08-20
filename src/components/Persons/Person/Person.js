878c7b44c63a788e500dfc820a941a09df220c48
VIBHOR94 <vibhorgupta2008@aol.in>
2018-08-20 17:03:35 +0530

    Turn stateless component into stateful component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,14 @@
-import React  from 'react'
+import React, { Component }  from 'react'
 import classes from './Person.css'
 
-const person = (props) => {
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>I'm a {props.name} and I'm {props.age} years old</p>
-      <input type='text' onChange={props.changed} value={props.name}/>
-      <p>{props.children}</p>
-    </div>
-  )
-};
 
-export default person;
+class Person extends Component {
+  render(){
+    return <div className={classes.Person}>
+      <p onClick={this.props.click}>I'm a {this.props.name} and I'm {this.props.age} years old</p>
+      <input type='text' onChange={this.props.changed} value={this.props.name}/>
+      <p>{this.props.children}</p>
+    </div>
+  }
+}
+export default Person;
