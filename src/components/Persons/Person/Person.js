5a889105a2c7b8fe2c2465ae91ffb3bc83c40eff
VIBHOR94 <vibhorgupta2008@aol.in>
2018-08-21 01:35:54 +0530

    Add hoc with props in App.js and Person.js file

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, { Component }  from 'react'
 import classes from './Person.css'
+import WithClass from '../../../hoc/WithClass'
 
 
 class Person extends Component {
@@ -18,11 +19,11 @@ class Person extends Component {
 
   render(){
     console.log('[Person.js] insider render()')
-    return <div className={classes.Person}>
+    return <WithClass classes={classes.Person}>
       <p onClick={this.props.click}>I'm a {this.props.name} and I'm {this.props.age} years old</p>
       <input type='text' onChange={this.props.changed} value={this.props.name}/>
       <p>{this.props.children}</p>
-    </div>
+    </WithClass>
     // return [
     //   <p key='1' onClick={this.props.click}>I'm a {this.props.name} and I'm {this.props.age} years old</p>,
     //   <input key='2' type='text' onChange={this.props.changed} value={this.props.name}/>,
