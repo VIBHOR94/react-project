74a4936dbe70942f29371292d16765dcc36aa0cd
VIBHOR94 <vibhorgupta2008@aol.in>
2018-08-21 02:43:43 +0530

    Alternative way of updating state

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,8 @@ class App extends PureComponent {
         {id :'2', name: 'Manu', age: '29'},
         {id :'3', name: 'Stephanie', age: '26'}
       ],
-      showPersons: false
+      showPersons: false,
+      toggleClicked: 0
     }
   }
 
@@ -50,7 +51,12 @@ class App extends PureComponent {
 
   toggleShowPersons = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+    this.setState((prevState, props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1
+      }
+    });
   }
 
   switchChangeHandler = (event, id) => {
